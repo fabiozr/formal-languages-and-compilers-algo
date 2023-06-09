10dc2a8942df01c389934bb4039ba4ced2f0b925
Fábio Pereira dos Santos <fabio.santos@quintoandar.com.br>
2023-06-09 15:57:29 -0300

    feat: add to_nfa function header

--- grammars/regular.py
+++ grammars/regular.py
@@ -4,3 +4,7 @@ from .grammar import Grammar
 class RegularGrammar(Grammar):
     def __init__(self):
         super().__init__()
+
+    # (b.2) Conversão de GR para AFN
+    def to_nfa(self):
+        raise NotImplementedError
